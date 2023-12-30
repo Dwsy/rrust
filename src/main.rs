93cfcf5d9a9b56079089b32f61e00e7bc416c8b7
Dwsy <dwsycode@gmail.com>
2023-12-31 01:40:41 +0800

    dbg!

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
 #[derive(Debug)]
-struct File {
-    name: String,
-    data: Vec<u8>,
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
 fn main() {
-    let mut data = Vec::new();
-    data.push(1);
-    data.push(2);
-    data.push(3);
-
-    let f1 = File {
-        name: String::from("f1.txt"),
-        data: data,
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
     };
 
-    let f1_name = &f1.name;
-    let f1_length = f1.data.len();
-
-    println!("{:?}", f1);
-    println!("{} is {} bytes long", f1_name, f1_length);
+    dbg!(&rect1);
 }
