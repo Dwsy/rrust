870d44763f316156670e2450d43ac6fefc6ec4f0
Dwsy <dwsycode@gmail.com>
2023-12-30 20:02:29 +0800

    gives_ownership

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 fn main() {
-    let s = String::from("hello");  // s 进入作用域
+    let s1 = gives_ownership();         // gives_ownership 将返回值
+    // 移给 s1
+    println!("Memory address of s1: {:?}", s1.as_ptr());
+    let s2 = String::from("hello");     // s2 进入作用域
+    println!("Memory address of s2: {:?}", s2.as_ptr());
+    let s3 = takes_and_gives_back(s2);  // s2 被移动到
+    println!("Memory address of s3: {:?}", s3.as_ptr());
+    // takes_and_gives_back 中,
+    // 它也将返回值移给 s3
+} // 这里, s3 移出作用域并被丢弃。s2 也移出作用域，但已被移走，
+// 所以什么也不会发生。s1 移出作用域并被丢弃
 
-    takes_ownership(s);             // s 的值移动到函数里 ...
-    // ... 所以到这里不再有效
-    // println!("{}", s);
-    let x = 5;                      // x 进入作用域
+fn gives_ownership() -> String {             // gives_ownership 将返回值移动给
+    // 调用它的函数
 
-    makes_copy(x);                  // x 应该移动函数里，
-    // 但 i32 是 Copy 的，所以在后面可继续使用 x
-    println!("{}", x);
-} // 这里, x 先移出了作用域，然后是 s。但因为 s 的值已被移走，
-// 所以不会有特殊操作
+    let some_string = String::from("hello"); // some_string 进入作用域.
+    println!("Memory address of some_string: {:?}", some_string.as_ptr());
+    some_string                              // 返回 some_string 并移出给调用的函数
+}
 
-fn takes_ownership(some_string: String) { // some_string 进入作用域
-    println!("{}", some_string);
-} // 这里，some_string 移出作用域并调用 `drop` 方法。占用的内存被释放
+// takes_and_gives_back 将传入字符串并返回该值
+fn takes_and_gives_back(a_string: String) -> String { // a_string 进入作用域
 
-fn makes_copy(some_integer: i32) { // some_integer 进入作用域
-    println!("{}", some_integer);
-} // 这里，some_integer 移出作用域。不会有特殊操作
+    a_string  // 返回 a_string 并移出给调用的函数
+}
