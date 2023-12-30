b441b5b7fce40ee235cd25d1b0d1ad75d72a837c
Dwsy <dwsycode@gmail.com>
2023-12-30 21:50:04 +0800

    引用与解引用

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,8 @@
 fn main() {
-    let s1 = gives_ownership();         // gives_ownership 将返回值
-    // 移给 s1
-    println!("Memory address of s1: {:?}", s1.as_ptr());
-    let s2 = String::from("hello");     // s2 进入作用域
-    println!("Memory address of s2: {:?}", s2.as_ptr());
-    let s3 = takes_and_gives_back(s2);  // s2 被移动到
-    println!("Memory address of s3: {:?}", s3.as_ptr());
-    // takes_and_gives_back 中,
-    // 它也将返回值移给 s3
-} // 这里, s3 移出作用域并被丢弃。s2 也移出作用域，但已被移走，
-// 所以什么也不会发生。s1 移出作用域并被丢弃
+    let x = 5;
+    let y = &x;
 
-fn gives_ownership() -> String {             // gives_ownership 将返回值移动给
-    // 调用它的函数
-
-    let some_string = String::from("hello"); // some_string 进入作用域.
-    println!("Memory address of some_string: {:?}", some_string.as_ptr());
-    some_string                              // 返回 some_string 并移出给调用的函数
-}
-
-// takes_and_gives_back 将传入字符串并返回该值
-fn takes_and_gives_back(a_string: String) -> String { // a_string 进入作用域
-
-    a_string  // 返回 a_string 并移出给调用的函数
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+    assert_eq!(&x, y);
 }
