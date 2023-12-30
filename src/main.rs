c6a261b13840825abbc68e8fea25b9d110eae1ea
Dwsy <dwsycode@gmail.com>
2023-12-31 02:10:14 +0800

    枚举1

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
 #[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
+#[allow(dead_code)]
+enum PokerSuit {
+    Clubs,
+    Spades,
+    Diamonds,
+    Hearts,
 }
 
 fn main() {
-    let scale = 2;
-    let rect1 = Rectangle {
-        width: dbg!(30 * scale),
-        height: 50,
-    };
+    let heart = PokerSuit::Hearts;
+    let diamond = PokerSuit::Diamonds;
 
-    dbg!(&rect1);
+    print_suit(&heart);
+    print_suit(&diamond);
+
+    println!("{:?}", heart);
+    println!("{:?}", diamond);
+    println!("index {:?}",heart as i32);
+    println!("index {:?}",diamond as i32);
+
+}
+
+fn print_suit(card: &PokerSuit) {
+    // 需要在定义 enum PokerSuit 的上面添加上 #[derive(Debug)]，否则会报 card 没有实现 Debug
+    println!("{:?}",*card);
+    // println!("index {:?}",card as i32);
 }
