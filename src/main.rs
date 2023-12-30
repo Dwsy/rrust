1e6dba62d1cb6a6877109024185485017683a9a9
Dwsy <dwsycode@gmail.com>
2023-12-30 15:23:53 +0800

    1+2+3+...+100

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
-    let mut hello = "hello world";
-    println!("{}", hello);
-}
+    let sum:i32 = {
+        let mut i  = 0;
+        for j in 0..101 {
+            i += j
+        }
+         i
+    };
 
+    println!("1..100 sum eq : {}", sum);
+
+}
