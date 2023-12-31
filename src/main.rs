e122302969338e6a4685790317c73873464dff41
Dwsy <dwsycode@gmail.com>
2023-12-31 10:47:29 +0800

    match

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,12 @@
-enum Number {
-    Zero,
-    One,
-    Two,
-}
-
-enum Number1 {
-    Zero = 0,
-    One,
-    Two,
-}
-
-// C-like enum
-enum Number2 {
-    Zero = 0,
-    One = 1,
-    Two = 2,
-}
-
-
 fn main() {
-    // a enum variant can be converted to a integer by `as`
-    assert_eq!(Number::One as u8, Number1::One as u8);
-    assert_eq!(Number1::One as u8, Number2::One as u8);
-} 
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {:?}", y),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {:?}", x, y);
+}
