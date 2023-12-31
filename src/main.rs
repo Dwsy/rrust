3f6a6f3e7026a0981e3908b2b27ddb04f20a589c
Dwsy <dwsycode@gmail.com>
2023-12-31 12:26:06 +0800

    impl1

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 fn main() {
-    let x = Some(5);
-    let y = 10;
+    let circle = Circle::new(0.0, 0.0, 10.0);
+    let area = circle.area();
+    println!("{}", area);
 
-    match x {
-        Some(50) => println!("Got 50"),
-        Some(y) => println!("Matched, y = {:?}", y),
-        _ => println!("Default case, x = {:?}", x),
+}
+struct Circle {
+    x: f64,
+    y: f64,
+    radius: f64,
+}
+
+impl Circle {
+    // new是Circle的关联函数，因为它的第一个参数不是self，且new并不是关键字
+    // 这种方法往往用于初始化当前结构体的实例
+    fn new(x: f64, y: f64, radius: f64) -> Circle {
+        Circle {
+            x: x,
+            y: y,
+            radius: radius,
+        }
     }
 
-    println!("at the end: x = {:?}, y = {:?}", x, y);
+    // Circle的方法，&self表示借用当前的Circle结构体
+    fn area(&self) -> f64 {
+        std::f64::consts::PI * (self.radius * self.radius)
+    }
 }
