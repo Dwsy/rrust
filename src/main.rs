f5fcf043d3814f3d8c6efaa7191320969ced0a38
Dwsy <dwsycode@gmail.com>
2023-12-31 13:54:37 +0800

    debug1

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
-fn display_array<T: std::fmt::Debug, const N: usize>(arr: [T; N]) {
-    println!("{:?}", arr);
-}
-fn main() {
-    let arr: [i32; 3] = [1, 2, 3];
-    display_array(arr);
+use std::fmt;
 
-    let arr: [i32; 2] = [1, 2];
-    display_array(arr);
+fn main() {
+    let a = 1;
+    let b = 2;
+    println!("a: {}, b: {}", a, b);
+
+    let c = DebugTest { a: 1, b: 2 };
+    println!("c: {:?}", c);
+    println!("c: {:#?}", c);
+
+}
+
+struct DebugTest {
+    a: i32,
+    b: i32,
+}
+impl fmt::Debug for DebugTest {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "DebugTest {{ a: {}, b: {} }}", self.a, self.b)
+    }
 }
