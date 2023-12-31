cab53d4a204b450b731ff0e5632f8b8c518c477d
Dwsy <dwsycode@gmail.com>
2023-12-31 13:26:20 +0800

    pi

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,25 @@
 fn main() {
-    let user = User::new("Dwsy".to_string(), 18);
-    println!("name: {}", user.get_name());
-    println!("{}", user.age);
-    println!("{}", user.name);
-}
+    let mut collisions = 0;
+    let light = 1.0;
+    let heavy = 1000000.0;
 
-struct User {
-    name: String,
-    age: u32,
-}
+    let mut light_velocity = 0.0;
+    let mut heavy_velocity = -1.0;
 
-impl User {
-    fn new(name: String, age: u32) -> User {
-        User {
-            name,
-            age,
+    while heavy_velocity < light_velocity || light_velocity < 0.0 {
+        if heavy_velocity < light_velocity {
+            let new_light_velocity =
+                (light - heavy) / (light + heavy) * light_velocity
+                    + 2.0 * heavy / (light + heavy) * heavy_velocity;
+            heavy_velocity =
+                2.0 * light / (light + heavy) * light_velocity
+                    + (heavy - light) / (light + heavy) * heavy_velocity;
+            light_velocity = new_light_velocity;
+        } else {
+            light_velocity = -light_velocity;
         }
+        collisions += 1;
     }
-}
-impl User {
-    fn get_name(&self) -> &String {
-        &self.name
-    }
+
+    println!("Collisions: {}", collisions);
 }
