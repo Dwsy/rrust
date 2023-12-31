957f125c4dd3fb07c9a4685b1b33df7937aaef3c
Dwsy <dwsycode@gmail.com>
2024-01-01 01:37:41 +0800

    特征2

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,33 @@
-pub trait Summary {
-    fn summarize(&self) -> String;
-    fn new() -> Self;
+use std::ops::Add;
+
+// 为Point结构体派生Debug特征，用于格式化输出
+#[derive(Debug)]
+struct Point<T: Add<T, Output = T>> { //限制类型T必须实现了Add特征，否则无法进行+操作。
+x: T,
+    y: T,
 }
 
-pub struct Post {
-    pub title: String,
-    // 标题
-    pub author: String,
-    // 作者
-    pub content: String, // 内容
-}
+impl<T: Add<T, Output = T>> Add for Point<T> {
+    type Output = Point<T>;
 
-impl Summary for Post {
-    fn summarize(&self) -> String {
-        format!("文章{}, 作者是{}", self.title, self.author)
-    }
-    fn new() -> Self {
-        Post {
-            title: "".to_string(),
-            author: "".to_string(),
-            content: "".to_string(),
+    fn add(self, p: Point<T>) -> Point<T> {
+        Point{
+            x: self.x + p.x,
+            y: self.y + p.y,
         }
     }
 }
 
-pub struct Weibo {
-    pub username: String,
-    pub content: String,
-}
-
-impl Summary for Weibo {
-    fn summarize(&self) -> String {
-        format!("{}发表了微博{}", self.username, self.content)
-    }
-
-    fn new() -> Self {
-        todo!()
-    }
+fn add<T: Add<T, Output=T>>(a:T, b:T) -> T {
+    a + b
 }
 
 fn main() {
-    let mut post = Post::new();
-    post = Post {
-        title: String::from("Rust从入门到入门"),
-        author: "author".to_string(),
-        content: "content".to_string(),
-    };
+    let p1 = Point{x: 1.1f32, y: 1.1f32};
+    let p2 = Point{x: 2.1f32, y: 2.1f32};
+    println!("{:?}", add(p1, p2));
 
-    println!("{}", post.summarize());
+    let p3 = Point{x: 1i32, y: 1i32};
+    let p4 = Point{x: 2i32, y: 2i32};
+    println!("{:?}", add(p3, p4));
 }
