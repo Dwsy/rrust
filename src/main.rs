b5ad622a42c467f929d80e04c6809be040140c43
Dwsy <dwsycode@gmail.com>
2024-01-07 14:14:09 +0800

    add

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,40 @@
 use std::ops::Add;
 
-// 为Point结构体派生Debug特征，用于格式化输出
 #[derive(Debug)]
-struct Point<T: Add<T, Output = T>> { //限制类型T必须实现了Add特征，否则无法进行+操作。
-x: T,
-    y: T,
+struct User {
+    name: String,
+    seconds: i32,
 }
 
-impl<T: Add<T, Output = T>> Add for Point<T> {
-    type Output = Point<T>;
+impl User {
+    fn new(name: String, s: i32) -> Self {
+        User {
+            name,
+            seconds: s,
+        }
+    }
 
-    fn add(self, p: Point<T>) -> Point<T> {
-        Point{
-            x: self.x + p.x,
-            y: self.y + p.y,
+    fn add(&self, seconds1: i32) -> String {
+        let s = self.seconds + seconds1;
+        let name = format!("{}{}", self.name, s);
+        name
+    }
+}
+
+impl Add<i32> for User {
+    type Output = User;
+
+    fn add(self, rhs: i32) -> Self::Output {
+        User {
+            name: self.name,
+            seconds: self.seconds + rhs,
         }
     }
 }
 
-fn add<T: Add<T, Output=T>>(a:T, b:T) -> T {
-    a + b
-}
-
 fn main() {
-    let p1 = Point{x: 1.1f32, y: 1.1f32};
-    let p2 = Point{x: 2.1f32, y: 2.1f32};
-    println!("{:?}", add(p1, p2));
-
-    let p3 = Point{x: 1i32, y: 1i32};
-    let p4 = Point{x: 2i32, y: 2i32};
-    println!("{:?}", add(p3, p4));
+    let name = String::from("Dwsy");
+    let user = User::new(name, 18);
+    let user = user + 1;
+    println!("{:?}", user);
 }
