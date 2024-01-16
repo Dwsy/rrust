1a6ea1905694a3893ba7f3bca1be0cac3223b0b6
Dwsy <dwsycode@gmail.com>
2024-01-16 17:42:03 +0800

    string3

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,14 @@
-// FILL in the blanks
 fn main() {
-    // get a slice of String with reference: String -> &str
-    let mut s = String::from("hello, world");
+    let mut s = String::new();
+    s.push_str("hello");
 
-    let slice1: &str = s.as_mut(); // in two ways
-    assert_eq!(slice1, "hello, world");
+    let v = vec![104, 101, 108, 108, 111];
 
-    let slice2 = &s[0..5];
-    assert_eq!(slice2, "hello");
 
-    //Note! The type here cant be `&mut str` due to `push` is ONLY defined on String type and its mut reference: `&mut String` !
-    // So you can't use `s.as_mut_str()`
-    let slice3: &mut String = &mut s;
-    slice3.push('!');
-    assert_eq!(slice3, "hello, world!");
+    // 将字节数组转换成 String
+    let s1 = String::from_utf8(v).unwrap();
 
-    s=String::from("hello, world new");
-    println!("{}", s);
-    slice3.push_str(" new");
+    assert_eq!(s, s1);
 
     println!("Success!")
 }
