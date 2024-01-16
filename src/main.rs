b6c1e5a2e8f6360be09c25cd24ae4d9510f6bce9
Dwsy <dwsycode@gmail.com>
2024-01-16 17:36:39 +0800

    string2

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
+// FILL in the blanks
 fn main() {
-    let mut s: String = String::from("hello, ");
-    s.push_str("world");
-    s.push('!');
+    // get a slice of String with reference: String -> &str
+    let mut s = String::from("hello, world");
 
-    move_ownership(s.clone());
+    let slice1: &str = s.as_mut(); // in two ways
+    assert_eq!(slice1, "hello, world");
 
-    assert_eq!(s, "hello, world!");
+    let slice2 = &s[0..5];
+    assert_eq!(slice2, "hello");
+
+    //Note! The type here cant be `&mut str` due to `push` is ONLY defined on String type and its mut reference: `&mut String` !
+    // So you can't use `s.as_mut_str()`
+    let slice3: &mut String = &mut s;
+    slice3.push('!');
+    assert_eq!(slice3, "hello, world!");
+
+    s=String::from("hello, world new");
+    println!("{}", s);
+    slice3.push_str(" new");
 
     println!("Success!")
-}
-
-fn move_ownership(s: String) {
-    println!("ownership of \"{}\" is moved here!", s)
 }
