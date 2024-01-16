a21c16d5e936b83db778f06ec90d05f59d645290
Dwsy <dwsycode@gmail.com>
2024-01-16 17:30:06 +0800

    string1

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
-use std::collections::HashMap;
-
 fn main() {
-    let v1 = 10;
-    let mut m1 = HashMap::new();
-    m1.insert(v1, v1);
-    println!("v1 is still usable after inserting to hashmap : {}", v1);
+    let mut s: String = String::from("hello, ");
+    s.push_str("world");
+    s.push('!');
 
-    // &str implements Copy trait
-    let mut v2 = String::from("hello");
-    let mut m2 = HashMap::new();
-    m2.insert(v2, v1);
-    v2 = "world".parse().unwrap();
+    move_ownership(s.clone());
 
-    println!("m2 {:?}", m2.get(&v2));
-    println!("v2 is still usable after inserting to hashmap : {}", v2);
-    assert_eq!(v2, "world");
+    assert_eq!(s, "hello, world!");
+
+    println!("Success!")
+}
+
+fn move_ownership(s: String) {
+    println!("ownership of \"{}\" is moved here!", s)
 }
