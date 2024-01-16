2c8701e6606ea4362c278d9f88df3a468effa255
Dwsy <dwsycode@gmail.com>
2024-01-16 17:12:51 +0800

    hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,35 @@
-trait Draw {
-    fn draw(&self) -> String;
-}
-
-impl Draw for u8 {
-    fn draw(&self) -> String {
-        format!("u8: {}", *self)
-    }
-}
-
-impl Draw for f64 {
-    fn draw(&self) -> String {
-        format!("f64: {}", *self)
-    }
-}
-
-// 若 T 实现了 Draw 特征， 则调用该函数时传入的 Box<T> 可以被隐式转换成函数参数签名中的 Box<dyn Draw>
-fn draw1(x: Box<dyn Draw>) {
-    // 由于实现了 Deref 特征，Box 智能指针会自动解引用为它所包裹的值，然后调用该值对应的类型上定义的 `draw` 方法
-    x.draw();
-}
-
-fn draw2(x: &dyn Draw) {
-    x.draw();
-}
+// FILL in the blanks
+use std::collections::HashMap;
 
 fn main() {
-    let x = 1.1f64;
-    // do_something(&x);
-    let y = 8u8;
+    // type inference lets us omit an explicit type signature (which
+    // would be `HashMap<&str, u8>` in this example).
+    let mut player_stats = HashMap::new();
 
-    // x 和 y 的类型 T 都实现了 `Draw` 特征，因为 Box<T> 可以在函数调用时隐式地被转换为特征对象 Box<dyn Draw> 
-    // 基于 x 的值创建一个 Box<f64> 类型的智能指针，指针指向的数据被放置在了堆上
-    draw1(Box::new(x));
-    // 基于 y 的值创建一个 Box<u8> 类型的智能指针
-    draw1(Box::new(y));
-    draw2(&x);
-    draw2(&y);
+    // insert a key only if it doesn't already exist
+    player_stats.entry("health").or_insert(100);
+
+    assert_eq!(player_stats["health"], 100);
+
+    // insert a key using a function that provides a new value only if it
+    // doesn't already exist
+    player_stats.entry("health1").or_insert_with(random_stat_buff);
+    assert_eq!(player_stats["health"], 100);
+    assert_eq!(player_stats["health1"], 42);
+
+
+    // Ensures a value is in the entry by inserting the default if empty, and returns
+    // a mutable reference to the value in the entry.
+    let health = player_stats.entry("health").or_insert(50);
+    assert_eq!(health, &100);
+    *health -= 50;
+    assert_eq!(*health, 50);
+
+    println!("Success!")
+}
+
+fn random_stat_buff() -> u8 {
+    // could actually return some random value here - let's just return
+    // some fixed value for now
+    42
 }
