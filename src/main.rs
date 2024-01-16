175e44fddd43dd97886b12e8b8a7c7d4ed80b45f
Dwsy <dwsycode@gmail.com>
2024-01-16 17:44:49 +0800

    string4

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut s = String::new();
     s.push_str("hello");
 
     let v = vec![104, 101, 108, 108, 111];
 
-
     // 将字节数组转换成 String
-    let s1 = String::from_utf8(v).unwrap();
+    let s1 = String::from_utf8(v)?;
 
     assert_eq!(s, s1);
 
-    println!("Success!")
+    println!("Success!");
+
+    Ok(())
 }
