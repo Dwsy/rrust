93ad3aff7ceaea885c857183bb3914d7d0dec22a
Dwsy <dwsycode@gmail.com>
2024-01-17 17:38:43 +0800

    enum1

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
-fn main() {
-    let mut array = [1, 2, 3, 4, 5];
-    let ptr = array.as_mut_ptr();
-    println!("ptr = {:p}", ptr);
-    let add1 = unsafe { ptr.add(0) };
-    println!("add1 = {:p}", add1);
-
-    unsafe {
-        *add1 = 6;
-        println!("array[5] = {}", *ptr);
-    }
+#[derive(Debug)]
+enum Number {
+    Zero,
+    One,
+    Two,
 }
+
+enum Number1 {
+    Zero = 0,
+    One,
+    Two,
+}
+
+// C-like enum
+enum Number2 {
+    Zero = 0,
+    One = 1,
+    Two = 2,
+}
+
+
+fn main() {
+    println!("{:?}", Number::One);
+    // a enum variant can be converted to a integer by `as`
+    assert_eq!(Number::One as u8, Number1::One as u8);
+    assert_eq!(Number1::One as u8, Number2::One as u8);
+} 
