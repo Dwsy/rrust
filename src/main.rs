18f7dc78714ca402c73991c9c253ef0dadfb57e1
Dwsy <dwsycode@gmail.com>
2024-01-17 17:10:32 +0800

    vec3 unsage

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
 // FIX the errors
-fn main() {
-    let mut v = vec![1, 2, 3];
+    fn main() {
+        let mut v = vec![1, 2, 3];
+        let mut v2 = vec![1, 2, 3];
 
-    let slice1 = &v[..];
-    // out of bounds will cause a panic
-    // You must use `v.len` here
-    let slice2 = &v[0..v.len()];
+        println!("v1 p:{:p} v2 p:{:p}", v.as_ptr(), v2.as_ptr());
 
-    assert_eq!(slice1, slice2);
+        let slice1 = &v[..];
+        // out of bounds will cause a panic
+        // You must use `v.len` here
+        let slice2 = &v[0..v.len()];
 
-    // slice are read only
-    // Note: slice and &Vec are different
-    let vec_ref: &mut Vec<i32> = &mut v;
-    (*vec_ref).push(4);
-    let slice3 = &mut v[0..];
+        assert_eq!(slice1, slice2);
 
-    assert_eq!(slice3, &[1, 2, 3, 4]);
+        // slice are read only
+        // Note: slice and &Vec are different
+        let vec_ref: &mut Vec<i32> = &mut v;
+        (*vec_ref).push(4);
+        v2.push(888);
+        let slice3 = &mut v[0..];
 
-    println!("Success!")
+        assert_eq!(slice3, &[1, 2, 3, 4]);
+
+        unsafe {
+            let slice4 = slice3.get_unchecked_mut(0..100);
+            println!("{:?}", slice4);
+        }
+        println!("{:?}", v2);
+
+        println!("\n-------");
+        println!("v1 p:{:p} v2 p:{:p}", v.as_ptr(), v2.as_ptr());
+        println!("Success!")
 }
