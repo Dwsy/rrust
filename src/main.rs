9e8f52fc212b5afbf30394f449243988ed30d0f2
Dwsy <dwsycode@gmail.com>
2024-01-17 17:34:25 +0800

    vec6

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,12 @@
-// FIX the errors
 fn main() {
-    let mut vec1 = Vec::with_capacity(2);
-    let mut vec2 = Vec::with_capacity(2);
-    vec1.push(1);
-    vec1.push(2);
-    vec1.push(3);
-    vec1.push(4);
-
-    vec2.push(5);
-    vec2.push(6);
-    vec2.push(7);
-    vec2.push(8);
-
-    let slice3 = &mut vec1[0..];
-
+    let mut array = [1, 2, 3, 4, 5];
+    let ptr = array.as_mut_ptr();
+    println!("ptr = {:p}", ptr);
+    let add1 = unsafe { ptr.add(0) };
+    println!("add1 = {:p}", add1);
 
     unsafe {
-        let slice4 = slice3.get_unchecked_mut(0..8);
-        println!("{:?}", slice4);
-
-        println!("{:p},{:p}", &vec1,&vec2);
+        *add1 = 6;
+        println!("array[5] = {}", *ptr);
     }
 }
