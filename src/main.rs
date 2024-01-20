1639cf2ac521e3c6a13870890ba95640f2f22c3d
Dwsy <dwsycode@gmail.com>
2024-01-20 22:45:02 +0800

    args collect

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
-use std::fs::File;
-use std::io::Write;
+use std::env;
 
-fn main() -> std::io::Result<()> {
-    // 打开文件，如果文件不存在则创建它
-    let mut file = File::create("hello.txt")?;
-
-    // 定义要写入的字符串
-    let contents = "Hello, world!";
-
-    // 将字符串写入文件
-    file.write_all(contents.as_bytes())?;
-
-    Ok(())
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    dbg!(args);
 }
