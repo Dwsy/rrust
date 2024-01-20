3b2fa9dee8b062a8d43189cfefb70d1cc9b22c94
Dwsy <dwsycode@gmail.com>
2024-01-20 23:02:24 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+
+    let query = if args.len() > 1 {
+        &args[1]
+    } else {
+        "Explosion"
+    };
+    let file_path = if args.len() > 2 {
+        &args[2]
+    } else {
+        "Explosion.txt"
+    };
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
