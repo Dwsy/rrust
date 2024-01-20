42326a180e49fe1be3794ec9a1db5a7fe57cc69e
Dwsy <dwsycode@gmail.com>
2024-01-20 23:06:55 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,23 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    let config = parse_config(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
+}
+#[derive()]
+struct Config<'a> {
+    query: &'a str,
+    file_path: &'a str,
+}
+
+fn parse_config(args: &[String]) -> Config {
     let query = if args.len() > 1 {
         &args[1]
     } else {
@@ -13,11 +30,8 @@ fn main() {
     } else {
         "Explosion.txt"
     };
-
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
-
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
+    Config {
+        query: query,
+        file_path: file_path,
+    }
 }
