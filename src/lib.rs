94f4b0f3637e75ee1147b7ad8635594415534fda
Dwsy <dwsycode@gmail.com>
2024-01-20 23:32:49 +0800

    漆黑

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+use colored::Colorize;
+
+pub fn search_text(contents: &str, config: &Config) -> () {
+    contents.lines().for_each(|line| {
+        if line.contains(config.query) {
+            highlight_search(line, config.query);
+        }
+    });
+}
+
+pub fn highlight_search(text: &str, search: &str) {
+    let highlighted = text.replace(search, &format!("{}", search.red()));
+    println!("{}", highlighted);
+}
+#[derive()]
+pub struct Config<'a> {
+    pub query: &'a str,
+    pub file_path: &'a str,
+}
+
+impl<'a> Config<'a> {
+    pub fn new(args: &'a Vec<String>) -> Self {
+        let query = if args.len() > 1 { &args[1] } else { "漆黑" };
+        let file_path = if args.len() > 2 {
+            &args[2]
+        } else {
+            "Explosion.txt"
+        };
+        Self { query, file_path }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use colored::*;
+use minigrep::*;
 use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,7 +7,7 @@ fn main() {
     // } else if args.len() > 3 {
     //     panic!("Not enough arguments");
     // }
-    let config = Config::new(&args);
+    let config = minigrep::Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -15,31 +15,5 @@ fn main() {
     let contents =
         fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
-    contents.lines().for_each(|line| {
-        if line.contains(config.query) {
-            highlight_search(line, config.query);
-        }
-    });
-}
-
-fn highlight_search(text: &str, search: &str) {
-    let highlighted = text.replace(search, &format!("{}", search.red()));
-    println!("{}", highlighted);
-}
-#[derive()]
-struct Config<'a> {
-    query: &'a str,
-    file_path: &'a str,
-}
-
-impl<'a> Config<'a> {
-    fn new(args: &'a Vec<String>) -> Self {
-        let query = if args.len() > 1 { &args[1] } else { "漆黑" };
-        let file_path = if args.len() > 2 {
-            &args[2]
-        } else {
-            "Explosion.txt"
-        };
-        Self { query, file_path }
-    }
+    search_text(&contents, &config);
 }
