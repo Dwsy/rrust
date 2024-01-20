20e122ee3c412801fab55b0e2ac27eb696d15a33
Dwsy <dwsycode@gmail.com>
2024-01-20 18:59:49 +0800

    life cycle2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-fn main() {
-    println!("{}", max(&1, &2));
+fn longest(x: &str, y: &str) -> String {
+    if x.len() > y.len() {
+        x.to_string()
+    } else {
+        y.to_string()
+    }
 }
 
-fn max<'a, T: PartialOrd>(foo: &'a T, bar: &'a T) -> &'a T {
-    if foo > bar {
-        foo
-    } else {
-        bar
+fn main() {
+    let string1 = String::from("long string is long");
+    let result;
+    {
+        let string2 = String::from("xoooooooooooooooooooooooooooooyz");
+        result = longest(string1.as_str(), string2.as_str());
     }
+    println!("The longest string is {}", result);
 }
