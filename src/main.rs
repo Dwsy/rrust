0be91b29b423e8533f9a697c4e8a3551bc7ef264
Dwsy <dwsycode@gmail.com>
2024-01-20 23:36:10 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,3 @@
-use std::fs::File;
-use std::io::Write;
+mod test;
 
-fn main() -> std::io::Result<()> {
-    // 打开文件，如果文件不存在则创建它
-    let mut file = File::create("hello.txt")?;
-
-    // 定义要写入的字符串
-    let contents = "Hello, world!";
-
-    // 将字符串写入文件
-    file.write_all(contents.as_bytes())?;
-
-    Ok(())
-}
+fn main() {}
