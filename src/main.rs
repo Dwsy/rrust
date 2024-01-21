ae036a5a363ab3761ee519cc87d3b6e890a2fff3
Dwsy <dwsycode@gmail.com>
2024-01-21 13:01:35 +0800

    life reborrow

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
+#[derive(Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Point {
+    fn move_to(&mut self, x: i32, y: i32) {
+        self.x = x;
+        self.y = y;
+    }
+}
+
 fn main() {
-    let mut s = String::from("hello");
+    let mut p = Point { x: 0, y: 0 };
+    let r = &mut p;
+    // reborrow! 此时对`r`的再借用不会导致跟上面的借用冲突
+    let rr: &Point = &*r;
 
-    let r1 = &s;
-    let r2 = &s;
-    println!("{} and {}", r1, r2);
-    // 新编译器中，r1,r2作用域在这里结束
+    // 再借用`rr`最后一次使用发生在这里，在它的生命周期中，我们并没有使用原来的借用`r`，因此不会报错
+    println!("{:?}", rr);
 
-    let r3 = &mut s;
-    println!("{}", r3);
+    // 再借用结束后，才去使用原来的借用`r`
+    r.move_to(10, 10);
+    println!("{:?}", r);
 }
