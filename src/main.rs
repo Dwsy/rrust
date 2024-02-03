e89ea6ae0679fe1897bd7e9d8b078a68aea9a9a7
Dwsy <dwsycode@gmail.com>
2024-02-03 17:52:41 +0800

    arc1

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use std::rc::Rc;
+use std::sync::Arc;
+use std::thread;
+
 fn main() {
-    let a = Rc::new(String::from("test ref counting"));
-    println!("count after creating a = {}", Rc::strong_count(&a));
-    let b =  Rc::clone(&a);
-    println!("count after creating b = {}", Rc::strong_count(&a));
-    {
-        let c =  Rc::clone(&a);
-        println!("count after creating c = {}", Rc::strong_count(&c));
+    let s = Arc::new(String::from("多线程漫游者"));
+    for _ in 0..10 {
+        let s = Arc::clone(&s);
+        let handle = thread::spawn(move || {
+            println!("{}", s)
+        });
     }
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
