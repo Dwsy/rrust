858cf80bbbb71ff450d20774f1bb58f92dc273f0
Dwsy <dwsycode@gmail.com>
2024-02-03 18:11:28 +0800

    RefCell1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use std::cell::Cell;
+use std::cell::RefCell;
+use std::rc::Rc;
 fn main() {
-    let c = Cell::new("asdf");
+    let s = Rc::new(RefCell::new("我很善变，还拥有多个主人".to_string()));
 
-    let one = c.get();
-    c.set("qwer");
-    let two = c.get();
-    println!("{},{}", one, two);
+    let s1 = s.clone();
+    let s2 = s.clone();
+    let s3 = s.clone();
+
+    // let mut s2 = s.borrow_mut();
+    s2.borrow_mut().push_str(", oh yeah!");
+
+    println!("{:?}\n{:?}\n{:?}", s, s1, s2);
 }
