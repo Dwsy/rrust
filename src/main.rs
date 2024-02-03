9218f3d36caeb8810fc5696c468aa21436eb2c01
Dwsy <dwsycode@gmail.com>
2024-02-03 18:40:39 +0800

    list

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,44 @@
-use std::cell::Cell;
+use crate::List::{Cons, Nil};
+use std::cell::RefCell;
+use std::rc::Rc;
 
-fn retain_even(nums: &mut Vec<i32>) {
-    let slice: &[Cell<i32>] = Cell::from_mut(&mut nums[..])
-        .as_slice_of_cells();
-
-    let mut i = 0;
-    for num in slice.iter().filter(|num| is_even(num.get())) {
-        slice[i].set(num.get());
-        i += 1;
-    }
-
-    nums.truncate(i);
+#[derive(Debug)]
+enum List {
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
 }
 
-fn is_even(i: i32) -> bool {
-    i % 2 == 0
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None,
+        }
+    }
 }
 
 fn main() {
-    let mut nums = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    retain_even(&mut nums);
-    println!("{:?}", nums);
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+    println!("a的初始化rc计数 = {}", Rc::strong_count(&a));
+    println!("a指向的节点 = {:?}", a.tail());
+
+    // 创建`b`到`a`的引用
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+    println!("在b创建后，a的rc计数 = {}", Rc::strong_count(&a));
+    println!("b的初始化rc计数 = {}", Rc::strong_count(&b));
+    println!("b指向的节点 = {:?}", b.tail());
+
+    // 利用RefCell的可变性，创建了`a`到`b`的引用
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("在更改a后，b的rc计数 = {}", Rc::strong_count(&b));
+    println!("在更改a后，a的rc计数 = {}", Rc::strong_count(&a));
+
+    // 下面一行println!将导致循环引用
+    // 我们可怜的8MB大小的main线程栈空间将被它冲垮，最终造成栈溢出
+    // println!("a next item = {:?}", a.tail());
 }
