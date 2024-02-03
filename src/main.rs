2c581949fb505852008572d94a4b0c4b7dd877e8
Dwsy <dwsycode@gmail.com>
2024-02-03 17:47:15 +0800

    rc1

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use std::fmt::Debug;
-
-fn print_it<T: Debug + 'static>(input: &T) {
-    println!("'static value passed in is: {:?}", input);
-}
-
+use std::rc::Rc;
 fn main() {
-    let i = 5;
-    drop(i);
-    print_it(&i);
+    let a = Rc::new(String::from("test ref counting"));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b =  Rc::clone(&a);
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c =  Rc::clone(&a);
+        println!("count after creating c = {}", Rc::strong_count(&c));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
